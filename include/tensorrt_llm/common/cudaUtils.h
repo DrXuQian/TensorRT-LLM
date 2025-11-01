6c2945e673d25a9315a82cfc092e24141b36e263
marlin-user <marlin@example.com>
2025-11-01 18:11:57 +0800

    Add cudaUtils.h with CUDA utility functions
    - Add CUDA_CHECK macro for error handling
    - Add getSMVersion and getMultiProcessorCount helpers
    - Add sync_check_cuda_error function

---
+++ include/tensorrt_llm/common/cudaUtils.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <iostream>
+#include <string>
+
+namespace tensorrt_llm {
+namespace common {
+
+// CUDA error checking macro
+#define CUDA_CHECK(call)                                                                                              \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        cudaError_t error = call;                                                                                     \
+        if (error != cudaSuccess)                                                                                     \
+        {                                                                                                              \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__ << " - " << cudaGetErrorString(error)      \
+                      << std::endl;                                                                                    \
+            exit(1);                                                                                                   \
+        }                                                                                                              \
+    } while (0)
+
+inline int getSMVersion() {
+    int device_id;
+    cudaGetDevice(&device_id);
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, device_id);
+    return prop.major * 10 + prop.minor;
+}
+
+inline int getMultiProcessorCount() {
+    int device_id;
+    cudaGetDevice(&device_id);
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, device_id);
+    return prop.multiProcessorCount;
+}
+
+inline void sync_check_cuda_error() {
+    cudaDeviceSynchronize();
+    cudaError_t error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        std::cerr << "CUDA error: " << cudaGetErrorString(error) << std::endl;
+        exit(1);
+    }
+}
+
+} // namespace common
+} // namespace tensorrt_llm
