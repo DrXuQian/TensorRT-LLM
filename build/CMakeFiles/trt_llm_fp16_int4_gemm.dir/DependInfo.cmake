640233bd0f49ae89db56c2825c3530571b8a68c1
marlin-user <marlin@example.com>
2025-11-01 18:17:24 +0800

    Successful build - kernel extraction works
    - Library compiles successfully
    - Simple test passes
    - Kernel runner instantiates correctly

---
+++ build/CMakeFiles/trt_llm_fp16_int4_gemm.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/qianxu/trt_llm_fp16_int4_extraction/src/fp16_int4_gemm_kernel.cu" "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/fp16_int4_gemm_kernel.cu.o" "gcc" "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/fp16_int4_gemm_kernel.cu.o.d"
+  "/home/qianxu/trt_llm_fp16_int4_extraction/src/missing_implementations.cpp" "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/missing_implementations.cpp.o" "gcc" "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/missing_implementations.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/trt_llm_fp16_int4_gemm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/fp16_int4_gemm_kernel.cu.o"
+  "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/fp16_int4_gemm_kernel.cu.o.d"
+  "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/missing_implementations.cpp.o"
+  "CMakeFiles/trt_llm_fp16_int4_gemm.dir/src/missing_implementations.cpp.o.d"
+  "libtrt_llm_fp16_int4_gemm.pdb"
+  "libtrt_llm_fp16_int4_gemm.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/trt_llm_fp16_int4_gemm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
